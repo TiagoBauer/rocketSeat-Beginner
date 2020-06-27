0ec09f9476791ecf8ccc55b84aa68e07cef7dacb
TiagoBauer <tiagopbauer@gmail.com>
2020-06-27 16:47:24 -0300

    [Classes][FEAT] Implemented the html and js for the second class, the objective here is to make a to-do list

---
+++ Classes/Class-002/todos.js
@@ -0,0 +1,54 @@
+function test(){
+    console.log("Test JS");
+}
+
+let listElements = document.querySelector("#app ul");
+let inputElement = document.querySelector("#app input");
+let buttonElement = document.querySelector("#app button");
+
+let toDos = JSON.parse(localStorage.getItem('list_todos')) || [];
+
+function renderTodo(){
+    listElements.innerHTML = "";
+    for(todo of toDos){
+        let todoElement = document.createElement("li");
+        let todoText = document.createTextNode(todo);
+
+        let linkElement = document.createElement('a');
+        linkElement.setAttribute('href', '#');
+
+        let pos = toDos.indexOf(todo);
+        linkElement.setAttribute('onclick', 'deleteTodo('+pos+')');
+
+        let linktext = document.createTextNode("Excluir");
+
+        linkElement.appendChild(linktext);
+        todoElement.appendChild(todoText);
+        todoElement.appendChild(linkElement);
+
+        listElements.appendChild(todoElement);
+    }
+}
+
+renderTodo();
+
+function addTodo(){
+    let todoText = inputElement.value;
+
+    toDos.push(todoText);
+    inputElement.value = "";
+    renderTodo();
+    saveToStorage()
+}
+
+function deleteTodo(pos){
+    toDos.splice(pos, 1);
+    renderTodo();
+    saveToStorage()
+}
+
+function saveToStorage(){
+
+    localStorage.setItem('list_todos', JSON.stringify(toDos));
+}
+
